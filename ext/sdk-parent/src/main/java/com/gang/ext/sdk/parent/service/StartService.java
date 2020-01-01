43d05b700f1742284a0188fe150e51f9a97edaaa
ant-black <1016930479@qq.com>
2020-01-01 23:28:46 +0800

    add some

---
+++ ext/sdk-parent/src/main/java/com/gang/ext/sdk/parent/service/StartService.java
@@ -0,0 +1,13 @@
+package com.gang.ext.sdk.parent.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Classname StartService
+ * @Description TODO
+ * @Date 2020/1/1 23:28
+ * @Created by zengzg
+ */
+@Component
+public class StartService {
+}
